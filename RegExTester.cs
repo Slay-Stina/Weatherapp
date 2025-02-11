04d7c0b30fa95cdf13aad86ce51b902b8d1b6c56
Slay Stina <august.hogbom@gmail.com>
2025-02-11 11:57:27 +0100

    regex to instance

--- RegExTester.cs
+++ RegExTester.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using Weatherapp.Models;
+using System.IO;
 
 namespace Weatherapp
 {
@@ -24,11 +26,32 @@ namespace Weatherapp
 
             MatchCollection matches = temps.Matches(text);
 
+            TempEntity tempEntity = new TempEntity();
 
-            foreach (Match match in matches)
-            {
-                Console.WriteLine(match.Value);
-            }
+            tempEntity.Date = DateTime.Parse(matches[0].Groups[1].Value);
+            tempEntity.IsIndoor = matches[0].Groups[10].Value.ToLower() == "inne" ? true : false;
+            tempEntity.Temperature = float.Parse(matches[0].Groups[11].Value.Replace('.', ','));
+            tempEntity.Humidity = int.Parse(matches[0].Groups[12].Value);
+
+            Console.WriteLine(tempEntity.Date + "\t" + tempEntity.Temperature + "\t" + tempEntity.IsIndoor + "\t" + tempEntity.Humidity);
+            Console.ReadKey();
+
+            //using(var db = new WeatherDbContext())
+            //{
+            //    foreach (Match match in matches)
+            //    {
+            //        TempEntity tempEntity = new TempEntity();
+
+            //        tempEntity.Date = DateTime.Parse(match.Groups[1].Value);
+            //        tempEntity.IsIndoor = match.Groups[10].Value.ToLower() == "inne" ? true : false;
+            //        tempEntity.Temperature = float.Parse(match.Groups[11].Value.Replace('.', ','));
+            //        tempEntity.Humidity = int.Parse(match.Groups[12].Value);
+
+            //        db.Add(tempEntity);
+
+            //    }
+            //    db.SaveChanges();
+            //}
         }
     }
 }
