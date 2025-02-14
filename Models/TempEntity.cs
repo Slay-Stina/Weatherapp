90230713c9661f791676293917b92d9713b0a409
Slay Stina <august.hogbom@gmail.com>
2025-02-14 12:11:32 +0100

    Flyttade metod

--- Models/TempEntity.cs
+++ Models/TempEntity.cs
@@ -18,6 +18,25 @@ namespace Weatherapp.Models
         public int Humidity { get; set; }
         public float MoldRisk { get; set; }
 
+        public static void PrintMoldRisk()
+        {
+            //RegExTester.TempToDB();
+
+            using (var db = new WeatherDbContext())
+            {
+                foreach (var entity in db.TempEntities.OrderByDescending(t => t.MoldRisk))
+                {
+                    //point.MoldRisk = point.CalculateMoldRisk();
+                    string riskLevel = entity.ClassifyMoldRisk(entity.MoldRisk);
+
+                    Console.WriteLine($"{entity.Date}: {(entity.IsIndoor ? "Inomhus" : "Utomhus")}, Temp: {entity.Temperature}°C, LF: {entity.Humidity}%");
+                    Console.WriteLine($"Mögelrisk: {entity.MoldRisk:F2}, Risknivå: {riskLevel}");
+                    Console.WriteLine();
+                }
+                //db.SaveChanges();
+            }
+        }
+
         public float CalculateMoldRisk()
         {
             if (Humidity < 80 || Temperature <= 5)
--- Program.cs
+++ Program.cs
@@ -7,21 +7,7 @@ namespace Weatherapp
     {
         static void Main(string[] args)
         {
-            //RegExTester.TempToDB();
-
-            using (var db = new WeatherDbContext())
-            {
-                foreach (var entity in db.TempEntities.OrderByDescending(t => t.MoldRisk))
-                {
-                    //point.MoldRisk = point.CalculateMoldRisk();
-                    string riskLevel = entity.ClassifyMoldRisk(entity.MoldRisk);
-
-                    Console.WriteLine($"{entity.Date}: {(entity.IsIndoor ? "Inomhus" : "Utomhus")}, Temp: {entity.Temperature}°C, LF: {entity.Humidity}%");
-                    Console.WriteLine($"Mögelrisk: {entity.MoldRisk:F2}, Risknivå: {riskLevel}");
-                    Console.WriteLine();
-                }
-                //db.SaveChanges();
-            }
+            TempEntity.PrintMoldRisk();
         }
     }
 }
